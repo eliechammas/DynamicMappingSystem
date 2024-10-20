f6f2cdbf67231c6b1da95f5d05182202feae0830
Elie Chammas <contact@eliechammas.me>
2024-10-20 11:28:41 +0200

    commit common use of ApiBaseController

--- DynamicMapping/Controllers/BaseApiController.cs
+++ DynamicMapping/Controllers/BaseApiController.cs
@@ -13,7 +13,11 @@ namespace DynamicMapping.Controllers
         protected void BaseInit()
         {
             if(this.Request == null || this.Request.HttpContext.Items["apiSession"] == null)
-            {}
+            {
+                /// common authorization check (Access to common information such as user ID or access token)
+                /// common methods (formatting response)
+                /// handling language and localization for responses
+            }
         }
     }
 }
