07c74837d0016eb58c45c298df6d37c4b29a59e5
Elie Chammas <contact@eliechammas.me>
2024-10-22 23:52:22 +0200

    Unit Test Enhancment

--- xUnitDM/MapHandler/MapHandlerTest.cs
+++ xUnitDM/MapHandler/MapHandlerTest.cs
@@ -11,6 +11,8 @@ using System.Text;
 using System.Threading.Tasks;
 using static DataModels.Sections.Internal.Room.DTO.RoomDto;
 using System.Text.Json;
+using DataModels.Sections.Internal.Room;
+using DataModels.Sections.External.Google.Room;
 
 namespace xUnitDM.MapHandler
 {
@@ -32,8 +34,15 @@ namespace xUnitDM.MapHandler
         {
             #region Arrange
             /// filling the right data to test the map method - Here Google Target Model
-            Object data = new { Id = 1, Code = "TEST123", Name = "Test_Room", Description = "This is the unit test room", Area = 100, Floor = "5" };
-            data = JsonSerializer.Deserialize<DataModels.Sections.Internal.Room.RoomModel>(JsonSerializer.Serialize<Object>(data), JsonSerializerOptions.Default);
+            DataModels.Sections.Internal.Room.RoomModel data = new DataModels.Sections.Internal.Room.RoomModel()
+                                                                {
+                                                                    Id = 1,
+                                                                    Code = "TEST123",
+                                                                    Name = "Test_Room",
+                                                                    Description = "This is the unit test room",
+                                                                    Area = 100,
+                                                                    Floor = "5"
+                                                                }; 
             string Source = BLL.Enums.EnumInternalModel.Room;
             string Target = _configuration.GetSection("ExternalModels").GetSection("Google").GetValue<string>("Room").ToString();
             #endregion
@@ -52,8 +61,13 @@ namespace xUnitDM.MapHandler
         {
             #region Arrange
             /// filling the right data to test the map method - Google Source Model & Data
-            Object data = new { Id = 1, Code = "TEST123", Name = "Test_Room", Description = "This is the unit test room", Area = 100, Floor = "5" };
-            data = JsonSerializer.Deserialize<DataModels.Sections.External.Google.Room.RoomModel>(JsonSerializer.Serialize<Object>(data), JsonSerializerOptions.Default);
+            DataModels.Sections.External.Google.Room.RoomModel data = new DataModels.Sections.External.Google.Room.RoomModel() 
+                                                                    { 
+                                                                        Name = "Googlr_Room", 
+                                                                        Description = "This is the unit test google room", 
+                                                                        Area = 10, 
+                                                                        Floor = "1" 
+                                                                    };
             string Source = _configuration.GetSection("ExternalModels").GetSection("Google").GetValue<string>("Room").ToString();
             string Target = BLL.Enums.EnumInternalModel.Room;
             #endregion
