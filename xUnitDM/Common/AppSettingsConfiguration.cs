e836a18b1099db5e85e2ba1522ebea615bc40785
Elie Chammas <contact@eliechammas.me>
2025-08-10 04:50:48 +0300

    commit file to fix pull push issue

---
+++ xUnitDM/Common/AppSettingsConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Castle.Core.Configuration;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IConfiguration = Microsoft.Extensions.Configuration.IConfiguration;
+
+namespace xUnitDM.Common
+{
+    internal static class AppSettingsConfiguration
+    {
+        private static IConfiguration _configuration;
+
+        public static IConfiguration GetConfiguration()
+        {
+            _configuration = new ConfigurationBuilder()
+                                            .SetBasePath(Directory.GetCurrentDirectory())
+                                            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                                            .Build();
+
+            return _configuration;
+        }
+
+    }
+}
---
+++ xUnitDM/Configuration/ConfigurationFixture.cs
@@ -0,0 +1,30 @@
+﻿using Castle.Core.Configuration;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IConfiguration = Microsoft.Extensions.Configuration.IConfiguration;
+
+namespace xUnitDM.Configuration
+{
+    public class ConfigurationFixture
+    {
+        public IConfiguration Configuration { get; }
+
+        public ConfigurationFixture() 
+        { 
+            var inMemorySettings = new Dictionary<string, string> 
+            {
+                
+            };
+
+            Configuration = new ConfigurationBuilder()
+                                            .SetBasePath(Directory.GetCurrentDirectory())
+                                            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                                            .Build();
+        }
+
+    }
+}
